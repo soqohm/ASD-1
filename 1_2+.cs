101185a245fc135d43fa00ecc5a869db653d5d5a
soqohm <soqohm@yandex.ru>
2023-12-13 06:46:42 +0500

    *
    *

---
+++ 1_2+.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node
+    {
+        public int value;
+        public Node next, prev;
+        public bool dummy;
+
+        public Node(int _value)
+        {
+            value = _value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class LinkedList2
+    {
+        public Node head { get { return dummy.next; } set { dummy.next = value; } }
+        public Node tail { get { return dummy.prev; } set { dummy.prev = value; } }
+
+        private Node dummy;
+
+        public LinkedList2()
+        {
+            dummy = new Node(0) { dummy = true };
+            dummy.next = dummy.prev = dummy;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            tail.next = _item;
+            _item.prev = tail;
+            _item.next = dummy;
+            tail = _item;
+        }
+
+        public Node Find(int _value)
+        {
+            Node node = head;
+
+            while (node != dummy)
+            {
+                if (node.value == _value) return node;
+                node = node.next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new List<Node>();
+            Node node = head;
+
+            while (node != dummy)
+            {
+                if (node.value == _value) nodes.Add(node);
+                node = node.next;
+            }
+            return nodes;
+        }
+
+        public bool Remove(int _value)
+        {
+            Node node = head;
+
+            while (node != dummy)
+            {
+                if (node.value == _value)
+                {
+                    node.prev.next = node.next;
+                    node.next.prev = node.prev;
+                    node.next = node.prev = null;
+                    return true;
+                }
+                node = node.next;
+            }
+            return false;
+        }
+
+        public void RemoveAll(int _value)
+        {
+            Node node = head;
+
+            while (node != dummy)
+            {
+                if (node.value == _value)
+                {
+                    node.next.prev = node.prev;
+                    node.prev = null;
+                    node = node.next;
+                    node.prev.next.next = null;
+                    node.prev.next = node;
+                }
+                else node = node.next;
+            }
+        }
+
+        public void Clear()
+        {
+            head = tail = dummy;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            Node node = head;
+
+            while (node != dummy)
+            {
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+            if (_nodeAfter == null)
+            {
+                _nodeToInsert.next = head;
+                head.prev = _nodeToInsert;
+                head = _nodeToInsert;
+                head.prev = dummy;
+            }
+            else
+            {
+                _nodeAfter.next.prev = _nodeToInsert;
+                _nodeToInsert.prev = _nodeAfter;
+                _nodeToInsert.next = _nodeAfter.next;
+                _nodeAfter.next = _nodeToInsert;
+            }
+        }
+
+    }
+}
--- 1_2.cs
+++ 1_2.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AlgorithmsDataStructures
+namespace AlgorithmsDataStructures2
 {
 
     public class Node
