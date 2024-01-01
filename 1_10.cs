b293c48f3b7ab3a16dbc2eebe69b65762fec36c3
soqohm <soqohm@yandex.ru>
2024-01-01 23:31:54 +0500

    *
    *

---
+++ 1_10.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class PowerSet<T>
+    {
+        public Dictionary<string, T> Pairs;
+
+        public PowerSet()
+        {
+            Pairs = new Dictionary<string, T>();
+        }
+
+        public int Size()
+        {
+            return Pairs.Count;
+        }
+
+        public void Put(T value)
+        {
+            var key = $"{value}";
+
+            if (!Pairs.ContainsKey(key))
+                Pairs.Add(key, value);
+        }
+
+        public bool Get(T value)
+        {
+            return Pairs.ContainsKey($"{value}");
+        }
+
+        public bool Remove(T value)
+        {
+            var key = $"{value}";
+
+            if (Pairs.ContainsKey(key))
+            {
+                Pairs.Remove(key);
+                return true;
+            }
+            return false;
+        }
+
+        public PowerSet<T> Intersection(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (var e in Pairs.Values)
+                if (set2.Get(e))
+                    result.Put(e);
+
+            return result;
+        }
+
+        public PowerSet<T> Union(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (var e in Pairs.Values)
+                result.Put(e);
+
+            foreach (var e in set2.Pairs.Values)
+                result.Put(e);
+
+            return result;
+        }
+
+        public PowerSet<T> Difference(PowerSet<T> set2)
+        {
+            var result = new PowerSet<T>();
+
+            foreach (var e in Pairs.Values)
+                if (!set2.Get(e))
+                    result.Put(e);
+
+            return result;
+        }
+
+        public bool IsSubset(PowerSet<T> set2)
+        {
+            foreach (var e in set2.Pairs.Values)
+                if (!Get(e))
+                    return false;
+
+            return true;
+        }
+    }
+}
--- 1_9.cs
+++ 1_9.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AlgorithmsDataStructures
+namespace AlgorithmsDataStructures14
 {
 
     public class NativeDictionary<T>
