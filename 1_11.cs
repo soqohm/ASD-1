8786fc452a7676bd0d00b35e8c8cc4d6ee32544e
soqohm <soqohm@yandex.ru>
2024-01-05 22:38:43 +0500

    *
    *

--- 1_11.cs
+++ 1_11.cs
@@ -7,48 +7,59 @@ namespace AlgorithmsDataStructures
     public class BloomFilter
     {
         public int filter_len;
-        private int[] _bits;
+        public int[] bits;
 
         public BloomFilter(int f_len)
         {
             filter_len = f_len;
-            _bits = new int[filter_len];
-            for (int i = 0; i < filter_len; i++) _bits[i] = 0;
+
+            var size = filter_len / 32;
+            if (filter_len % 32 != 0) size++;
+
+            bits = new int[size];
         }
 
         public int Hash1(string str1)
         {
-            var result = 0;
-
-            for (int i = 0; i < str1.Length; i++)
-            {
-                int code = (int)str1[i];
-                result = ((result * 17) + code) % filter_len;
-            }
-            return result;
+            return Hash(str1, 17);
         }
 
         public int Hash2(string str1)
         {
-            var result = 0;
-
-            for (int i = 0; i < str1.Length; i++)
-            {
-                int code = (int)str1[i];
-                result = ((result * 223) + code) % filter_len;
-            }
-            return result;
+            return Hash(str1, 223);
         }
 
         public void Add(string str1)
         {
-            _bits[Hash1(str1)] = 1;
-            _bits[Hash2(str1)] = 1;
+            Add_bit(Hash1(str1));
+            Add_bit(Hash2(str1));
         }
 
         public bool IsValue(string str1)
         {
-            return _bits[Hash1(str1)] == 1 && _bits[Hash2(str1)] == 1;
+            return IsValue_bit(Hash1(str1)) && IsValue_bit(Hash2(str1));
+        }
+
+        public int Hash(string str, int number)
+        {
+            var result = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int code = str[i];
+                result = ((result * number) + code) % filter_len;
+            }
+            return result;
+        }
+
+        public void Add_bit(int index)
+        {
+            bits[index / 32] |= 1 << (index % 32);
+        }
+
+        public bool IsValue_bit(int index)
+        {
+            return ((bits[index / 32] >> index % 32) & 1) == 1;
         }
     }
 }
